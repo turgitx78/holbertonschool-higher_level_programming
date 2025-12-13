61c976fd919edf30c3ceed662ce2a33468995401
turgitx78 <turgutazizli3@gmail.com>
2025-12-13 10:47:12 +0000

    Add 8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -3,4 +3,3 @@ str1 = "Python is an interpreted, interactive, object-oriented programming langu
 str2 = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
 str3 = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
 print(str1[39:67] + str2[107:112] + str3[:6])
-

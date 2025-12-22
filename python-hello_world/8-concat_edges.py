b141fab8c04bc51a2cda1a472fa14b7d16469396
turgitx78 <turgutazizli3@gmail.com>
2025-12-22 19:49:04 +0000

    8-concat_edges task

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-str1 = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
-str2 = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
-str3 = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
-print(str1[39:67] + str2[107:112] + str3[:6])
+str1 = "Holberton School"
+str2 = "Python"
+str3 = str1[:9] + " " + str1[10:]
+print(f"{str3} programming with {str2}")
+

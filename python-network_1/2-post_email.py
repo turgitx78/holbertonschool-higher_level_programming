a6137ecc6f26a4f5f57f8e14fd5120a59c233b0b
turgitx78 <turgutazizli3@gmail.com>
2026-01-05 15:53:44 +0000

    Guard main code to allow import for doc check

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
 """
-Takes a URL and an email, sends a POST request with the email,
+Takes a URL and an email, sends a POST request with the email as a parameter,
 and displays the response body decoded in utf-8.
 """
 import sys
 from urllib import request, parse
 
-url = sys.argv[1]
-email = sys.argv[2]
 
-data = parse.urlencode({'email': email}).encode('utf-8')
+def main():
+    url = sys.argv[1]
+    email = sys.argv[2]
 
-with request.urlopen(url, data) as response:
-    body = response.read()
+    data = parse.urlencode({'email': email}).encode('utf-8')
 
-print(body.decode('utf-8'))
+    with request.urlopen(url, data) as response:
+        print(response.read().decode('utf-8'))
+
+
+if __name__ == "__main__":
+    main()

40e8c879cc104dbb37f6807bf872caa05457c14b
turgitx78 <turgutazizli3@gmail.com>
2026-01-05 15:52:07 +0000

    Guard main code to allow import for doc check

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -6,6 +6,11 @@ of the X-Request-Id header from the response.
 import sys
 from urllib import request
 
-with request.urlopen(sys.argv[1]) as response:
-    headers = response.headers
-    print(headers.get('X-Request-Id'))
+
+def main():
+    with request.urlopen(sys.argv[1]) as response:
+        print(response.headers.get('X-Request-Id'))
+
+
+if __name__ == "__main__":
+    main()
